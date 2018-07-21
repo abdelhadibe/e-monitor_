99e9130f136a899b86f0c1e8c73ff266a1ecc77d
abdelhadi <abdelhadi2be@gmail.com>
2018-07-21 23:08:23 +0200

    publish to dz

--- modBusToMqqt_gateway.py
+++ modBusToMqqt_gateway.py
@@ -5,6 +5,7 @@ import json
 import paho.mqtt.client as mqtt
 import time 
 import datetime  
+import math
 from config import * 
 
 usb_port = '/dev/ttyUSB0' ; 
@@ -12,8 +13,9 @@ host = "localhost" ;
 port = 1883 ;
 
 energy_meters_topic = "/e-monitor/energy/";
+domoticzIn_topic = "domoticz/in"
 
-consumption_meter_id = 1 ; 
+consumption_meter_id = 3 ; 
 production_meter_id = 2; 
 
 def log_time(): 
@@ -57,16 +59,45 @@ class EnergyMeter(object):
 		if(metering_type == "consumption"):
 			data_1= json.dumps({ "idx" : voltage_idx, "nvalue" : 0, "svalue" :str(self._voltage_consumption) })
 			data_2= json.dumps({ "idx" : current_idx, "nvalue" : 0, "svalue" :str(self._current_consumption )})
-			data_3= json.dumps({ "idx" : energy_idx, "nvalue" : 0, "svalue" :str(self._total_active_energy_consumption)})
+			data_3= json.dumps({ "idx" : power_idx, "nvalue" : 0, "svalue" :str(self._active_power_consumption );1;2;3;4;5})
+			data_4= json.dumps({ "idx" : energy_idx, "nvalue" : 0, "svalue" :str(self._total_active_energy_consumption)})
 		
 		elif(metering_type == "production"):
 			data_1= json.dumps({ "idx" : voltage_idx, "nvalue" : 0, "svalue" :str(self._voltage_production) })
 			data_2= json.dumps({ "idx" : current_idx, "nvalue" : 0, "svalue" :str(self._current_production )})
-			data_3= json.dumps({ "idx" : energy_idx, "nvalue" : 0, "svalue" :str(self._total_active_energy_production)})
+			data_3= json.dumps({ "idx" : power_idx, "nvalue" : 0, "svalue" :str(self._active_power_consumption );1;2;3;4;5})
+			data_4= json.dumps({ "idx" : energy_idx, "nvalue" : 0, "svalue" :str(self._total_active_energy_production)})
 			
-		self._mqttc.publish("domoticz/in",data_1)
-		self._mqttc.publish("domoticz/in",data_2)
-		self._mqttc.publish("domoticz/in",data_3)
+		self._mqttc.publish(domoticzIn_topic,data_1)
+		self._mqttc.publish(domoticzIn_topic,data_2)
+		self._mqttc.publish(domoticzIn_topic,data_3)
+		self._mqttc.publish(domoticzIn_topic,data_4)
+
+
+	def publishTo_system(self):
+
+		data = dict();
+		consumption = dict();
+		production = dict() ; 
+
+		consumption["voltage"] = self._voltage_consumption ; 
+		consumption["current"] = self._current_consumption ; 
+		consumption["power"] = self._active_power_consumption ; 
+		consumption["energy"] = self._total_active_energy_consumption; 
+		production["voltage"] = self._voltage_production ; 
+		production["current"] = self._current_consumption ; 
+		production["power"] = self._active_power_production ; 
+		production["energy"] = self._total_active_energy_production ; 
+		data["consumption"] = consumption ;
+		data["production"] = production ; 
+
+		self._mqttc.publish(energy_meters_topic, json.dumps(data))
+		print data ; 
+
+
+
+
+
 
 	def __init__(self):
 		super(EnergyMeter, self).__init__();
@@ -82,10 +113,10 @@ class EnergyMeter(object):
 	def readEnergy_values(self):
 		#Values adress from datasheet
 		# Consumption Values
-		self._voltage_consumption = self._consumption_counter.read_float(0, functioncode =4, numberOfRegisters=2);
-		self._current_consumption = self._consumption_counter.read_float(6, functioncode =4, numberOfRegisters=2);
-		self._active_power_consumption = self._consumption_counter.read_float(12, functioncode =4, numberOfRegisters=2);
-		self._total_active_energy_consumption = self._consumption_counter.read_float(344, functioncode =4, numberOfRegisters=2);
+		self._voltage_consumption = round(self._consumption_counter.read_float(0, functioncode =4, numberOfRegisters=2), 1);
+		self._current_consumption = round(self._consumption_counter.read_float(6, functioncode =4, numberOfRegisters=2), 1);
+		self._active_power_consumption = round(self._consumption_counter.read_float(12, functioncode =4, numberOfRegisters=2), 1);
+		self._total_active_energy_consumption = round(self._consumption_counter.read_float(342, functioncode =4, numberOfRegisters=2), 4);
 		#PV Productionn values
 		"""
 		self._voltage_production = self._pv_procduction_counter.read_float(0, functioncode =4, numberOfRegisters=2);
@@ -105,16 +136,20 @@ class EnergyMeter(object):
 	def metering_system(self):
 		while True :
 			self.readEnergy_values();
+			self.publishTo_system();
 			self.publishToDomoticz("consumption",30,35,31,32);
-			#self.readEnergy_values(self._pv_procduction_counter,self._voltage_production, self._current_production, self._active_power_production, self._total_active_energy_production);
-			print self._voltage_consumption ;
-			print self._current_consumption ;
-			print self._total_active_energy_consumption ; 
+
+			print ("Voltage = {} \n".format(self._voltage_consumption) );
+			print ("Current = {} \n".format(self._current_consumption) );
+			print ("Power = {} \n".format(self._active_power_consumption )); 
+			print ("Energy = {} \n".format(self._total_active_energy_consumption));
+			print("------------------------------------------------\n")
 			time.sleep(4);
 
 def main():
-	met = EnergyMeter();
-	met.runEnergy_meter();
+	totalEnergy_meter = EnergyMeter();
+	totalEnergy_meter.runEnergy_meter();
+
 	while True: 
 		pass ;
 
